96ca63cc70deed5d8e823f712116a4a386bdc32f
Amelie Oller <amelieoller@gmail.com>
2020-09-12 09:59:38 -0700

    Create formatNames util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,29 @@
 export const collectIdsAndData = (doc) => {
   return { id: doc.id, ...doc.data() };
 };
+
+export const formatNames = (namesArr, category) => {
+  let string = "";
+
+  if (namesArr.length === 1) {
+    string = namesArr[0];
+  } else if (namesArr.length === 2) {
+    string = `${namesArr[0]} and ${namesArr[1]}`;
+  } else {
+    for (let i = 0; i < namesArr.length; i++) {
+      const el = namesArr[i];
+
+      if (i + 1 === namesArr.length) {
+        string += `and ${el}`;
+      } else {
+        string += `${el}, `;
+      }
+    }
+  }
+
+  if (category) {
+    string += `'s ${category}`;
+  }
+
+  return string;
+};
